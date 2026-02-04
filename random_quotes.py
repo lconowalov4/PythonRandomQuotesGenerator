5a78ff0a85b0c0c76909517af86d66df6296dabb
lconowalov4 <rw6lfe@gmail.com>
2026-02-04 21:27:50 +0300

    Added a menu and part of the main function of the app

--- random_quotes.py
+++ random_quotes.py
@@ -1,7 +1,7 @@
 import requests
 import pyperclip
 
-#function to fecth a random quote from the API
+#Function to fecth a random quote from the API
 def fetchRandomQuote():
     try:
         response = requests.get('https://api.quotable.io/random')
@@ -14,3 +14,25 @@ def fetchRandomQuote():
     except requests.exceptions.RequestException as e:
         print(f'An error occured: {e}')
         return None
+
+#Function to display the menu
+def displayMenu():
+    print('\n Welcome to the Quote Generator written in Python')
+    print('\n 1. Generate a new random quote')
+    print('\n 2. Copy the quote to clipboard')
+    print('\n 3. Exit')
+
+#Main application function
+def runQuoteGenerator():
+    currentQuote = None
+
+    while True:
+        displayMenu()
+        choice = input('Choose an option (1-3): ')
+
+        if choice == '1':
+            currentQuote = fetchRandomQuote()
+            if currentQuote:
+                print(f'Quote: {currentQuote['quotes']}')
+                print(f'Quote: {currentQuote['quotes']}')
+            
