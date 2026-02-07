60f7bdfce884caeff472b012f5612981d57fa92e
lconowalov4 <rw6lfe@gmail.com>
2026-02-07 14:57:07 +0300

    The main part of app has been improved and bugs have been fixed

--- random_quotes.py
+++ random_quotes.py
@@ -4,10 +4,10 @@ import pyperclip
 #Function to fecth a random quote from the API
 def fetchRandomQuote():
     try:
-        response = requests.get('https://api.quotable.io/random')
+        response = requests.get('https://api.quotable.io/random', verify = False)
         if response.status_code == 200:
             data = response.json()
-            return {'quotes':data['content'], 'author': data['author']}
+            return {'quote':data['content'], 'author': data['author']}
         else:
             print('Error fetching quote. Try again later')
             return None
@@ -33,6 +33,20 @@ def runQuoteGenerator():
         if choice == '1':
             currentQuote = fetchRandomQuote()
             if currentQuote:
-                print(f'Quote: {currentQuote['quotes']}')
-                print(f'Quote: {currentQuote['quotes']}')
-            
+                print(f"Quote: {currentQuote['quote']}")
+                print(f"Author: {currentQuote['author']}")
+        elif choice == '2':
+            if currentQuote:
+                quoteText = f"{currentQuote['quote']} - {currentQuote['author']}"
+                pyperclip.copy(quoteText)
+                print('Your quote has been copied to the clipboard')
+            else:
+                print('Generate a quote first.')
+        elif choice == '3':
+            print('Thank you for your time. Goodbye!')
+            break
+        else:
+            print('You need to enter something valid')
+
+#Run the app
+runQuoteGenerator()
